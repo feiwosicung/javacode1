eabd26f46101e1aaf7a18dd1b1c6e5fdb702e1b5
feiwosicung <1264748472@qq.com>
2018-11-07 23:31:30 +0800

    mmmeeeeee commit

--- heibernate_test001/src/Test.java
+++ heibernate_test001/src/Test.java
@@ -5,12 +5,9 @@ public class Test {
 	@org.junit.Test
 	public void test() {
 		fail("Not yet implemented");
-<<<<<<< HEAD
-		ÎáÎÊÎÞÎªÎ½;
-=======
-		sys;
-		tttt;;;;
->>>>>>> 3725358cee1ec2b87826efc7625e9d10e10513de
+
+		
+
 	}
 
 }
